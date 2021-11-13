c19ce148297d4a81db6c832c0a7dc249d4b5335e
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-11-13 00:47:22 -0500

    se ingresa google service y base de datos firebase

---
+++ app/src/main/java/com/project_moviles2/shop_homesweethome/actualizarUsuario.java
@@ -0,0 +1,14 @@
+package com.project_moviles2.shop_homesweethome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actualizarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_usuario);
+    }
+}
